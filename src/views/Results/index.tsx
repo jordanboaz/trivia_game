acaa6cca212ed60477a50afd5519808ac8439075
Jordan <jordanboazr@gmail.com>
2020-11-10 19:01:40 -0300

    Adds result screen

--- src/views/Results/index.tsx
+++ src/views/Results/index.tsx
@@ -1,13 +1,42 @@
-import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useDispatch } from 'react-redux';
+import { cleanQuiz } from '../../store/quiz';
+import { useTypedSelector } from '../../store/useTypedSelector';
+import { Screen, QuestionListResult, Button } from '../../components';
 
-import { Container } from './styles';
+import {
+  Container,
+  ScoreContainer,
+  ScoreText,
+  QuestionListContainer,
+} from './styles';
 
-const Results: React.FC = () => (
-  <Container>
-    <Text>END!</Text>
-  </Container>
-);
+const Results: React.FC = () => {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const correct = useTypedSelector((state) => state.quiz.correct);
+  const incorrect = useTypedSelector((state) => state.quiz.incorrect);
+  const questionList = useTypedSelector((state) => state.quiz.questionList);
+
+  const onPlayAgain = () => {
+    dispatch(cleanQuiz());
+    navigation.navigate('Home');
+  };
+
+  return (
+    <Screen>
+      <Container>
+        <ScoreContainer>
+          <ScoreText>{`You scored:\n${correct.length}/${questionList.length}`}</ScoreText>
+        </ScoreContainer>
+        <QuestionListContainer>
+          <QuestionListResult questionList={questionList} correct={correct} incorrect={incorrect} />
+        </QuestionListContainer>
+        <Button text="Play Again" onPress={onPlayAgain} />
+      </Container>
+    </Screen>
+  );
+};
 
 export default Results;
--- src/views/Results/styles.ts
+++ src/views/Results/styles.ts
@@ -1,5 +1,24 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+  padding: 16px;
+  flex: 1;
+  align-items: center;
+  /* background: #dedede; */
 
+`;
+
+export const ScoreContainer = styled.View``;
+
+export const ScoreText = styled.Text`
+font-size: 20px;
+text-align: center;
+line-height: 30px;
+margin-bottom: 20px;
+color: #3462e0;
+font-weight: bold;
+`;
+
+export const QuestionListContainer = styled.View`
+flex: 1;
 `;
